f0961fcde79cfd9328edab887e0629f0223b94ad
Tomas Tylecek <justspd@gmail.com>
2012-10-03 18:02:06 +0200

    zmeny

--- pg1/pg1/Camera.cpp
+++ pg1/pg1/Camera.cpp
@@ -1,17 +1,14 @@
-#include "StdAfx.h"
-#include "Camera.h"
-
+#include "stdafx.h"
 
 Camera::Camera(void)
 {
 
 }
 
-Camera(int width, int height, float fov_y, Vector3 eye, Vector3 up, Vector3 x, Vector3 y, Vector3 z)
+Camera::Camera(int width, int height, float fov_y, Vector3 eye, Vector3 up, Vector3 x, Vector3 y, Vector3 z);
 {
 
 }
-
 
 Camera::~Camera(void)
 {
