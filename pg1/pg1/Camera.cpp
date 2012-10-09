9853000e53a467e68c77327291b42e6b47eaf028
Tomas Tylecek <justspd@gmail.com>
2012-10-09 22:59:36 +0200

    novinky

--- pg1/pg1/Camera.cpp
+++ pg1/pg1/Camera.cpp
@@ -5,20 +5,40 @@ Camera::Camera(void)
 
 }
 
-Camera::Camera(int width, int height, float fov_y, Vector3 eye, Vector3 up, Vector3 x, Vector3 y, Vector3 z)
+Camera::Camera(int width, int height, float fov_y, Vector3 eye, Vector3 up, Vector3 z)
 {
-	width_ = width;
-	height_ = height;
-	fov_y_ = fov_y;
-	eye_ = eye;
-	up_ = up;
-	
-	axis_x_ = x;
+	_width = width;
+	_height = height;
+	_fov_y = fov_y;
+	_eye = eye;
+	_up = up;
 
-	axis_y_ = y;
-	axis_z_ = z;
+	_eye.Normalize();
+	_up.Normalize();
+
+	_ar = (float)width / (float) height;
+	_s = _ar / (2*tanf(_fov_y));
+	
+	_axis_z = z;
+	_axis_z.Normalize();
+	_axis_x = up.CrossProduct(z);
+	_axis_x.Normalize();
+	_axis_y = z.CrossProduct(_axis_y);
+	_axis_y.Normalize();
+
+
+	
 }
 
 Camera::~Camera(void)
 {
+}
+
+Ray* Camera::GenerateRay(float x, float y) {
+	
+	Vector3 dir;
+	dir.x = ((2.0f * (float)x) - (float)_width) / (float)_width * tanf(_fov_y);
+	dir.y = ((2.0f * (float)y) - (float)_height) / (float)_height * tanf(_fov_y);
+	dir.z = -1;
+	return NULL;
 }
--- pg1/pg1/Camera.h
+++ pg1/pg1/Camera.h
@@ -1,25 +1,29 @@
 #pragma once
 #include "stdafx.h"
+#include "Ray.h"
 
 class Camera
 {
 public:
 	Camera(void);
-	Camera(int width, int height, float fov_y, Vector3 eye, Vector3 up, Vector3 x, Vector3 y, Vector3 z);
+	Camera(int width, int height, float fov_y, Vector3 eye, Vector3 up, Vector3 z);
 	~Camera(void);
 
-	//Ray GenerateRay(float x, float y);
+	Ray* GenerateRay(float x, float y);
 
 private:
-	Vector3 eye_; //[WS]
-	int width_;
-	int height_;
+	Vector3 _eye; //[WS]
+	int _width;
+	int _height;
 
-	Vector3 axis_z_; // [WS]
-	Vector3 up_; // ukazuje, kde je nahoru
-	Vector3 axis_y_; // axis_z_ X x    znormlaizovat
-	Vector3 axis_x_; // axis_y_ X axis_z_     znormalizovat
+	Vector3 _axis_z; // [WS]
+	Vector3 _up; // ukazuje, kde je nahoru
+	Vector3 _axis_y; // axis_z_ X x    znormlaizovat
+	Vector3 _axis_x; // axis_y_ X axis_z_     znormalizovat
 
-	float fov_y_; // [rad] sirka zaberu kamery
+	float _fov_y; // [rad] sirka zaberu kamery
+
+	float _s;
+	float _ar;
 };
 
